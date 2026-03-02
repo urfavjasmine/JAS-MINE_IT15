bb3ab92803f30237d49bf8009cc1452112fd9d6d
urfavjasmine <jasminetampus30@gmail.com>
2026-03-03 02:25:28 +0800

    updated fix

--- Program.cs
+++ Program.cs
@@ -35,7 +35,14 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString, sqlOptions =>
+    {
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(10),
+            errorNumbersToAdd: null);
+        sqlOptions.CommandTimeout(60);
+    }));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
