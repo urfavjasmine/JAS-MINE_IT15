862b492f325be5b1e78de87ef63e53f0bcd42f1b
urfavjasmine <jasminetampus30@gmail.com>
2026-03-05 01:50:01 +0800

    Updated project files

---
+++ Data/Migrations/20260304173256_AddUserLimitColumn.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JAS_MINE_IT15.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserLimitColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DurationMonths",
+                table: "SubscriptionPlans",
+                type: "int",
+                nullable: false,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 12);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserLimit",
+                table: "SubscriptionPlans",
+                type: "int",
+                nullable: false,
+                defaultValue: 4);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserLimit",
+                table: "SubscriptionPlans");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DurationMonths",
+                table: "SubscriptionPlans",
+                type: "int",
+                nullable: false,
+                defaultValue: 12,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 1);
+        }
+    }
+}
